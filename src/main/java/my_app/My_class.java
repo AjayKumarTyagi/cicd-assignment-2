11493b187aa0f2df38b4584aa65d935c960fe8fb
Ajay Kumar Tyagi <ajaytyagi0209@gmail.com>
2020-11-19 13:32:12 +0530

    updated java file

--- src/main/java/my_app/My_class.java
+++ src/main/java/my_app/My_class.java
@@ -3,6 +3,6 @@ package my_app;
 public class My_class {
 	public static void main(String args[])
 	{
-		System.out.println("My java application");
+		System.out.println("My java1 application");
 	}
 }
